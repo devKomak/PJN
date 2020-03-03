aecd6640474ed17cacbde8c8bf03558dc4539db6
Konrad <komcio96@gmail.com>
2020-03-03 12:49:26 +0100

    2020-03-03

---
+++ scripts/script_1.R
@@ -0,0 +1,51 @@
+#włączanie bibliotek
+library(tm)
+
+#zmiana katalogu roboczego
+workDir <- "D:\\km193873\\pjn"
+setwd(workDir)
+
+#definicja katalogu projektu
+inputDir <- ".\\data"
+outputDir <- ".\\results"
+scriptsDir <- ".\\scripts"
+workspaceDir <- ".\\workspaces"
+
+#utworzenie katalogu wyjściowego
+dir.create(outputDir, showWarnings = TRUE)
+dir.create(workspaceDir, showWarnings = TRUE)
+
+#utworzenie korpusu dokumentów
+corpusDir <- paste(
+  inputDir, 
+  "\\",
+  "Literatura - streszczenia - oryginał",
+  sep = ""
+  )
+corpus <- VCorpus(
+  DirSource(
+    corpusDir,
+    pattern = "*.txt",
+    encoding = "UTF-8"
+    ),
+  readerControl = list(
+    language="pl_PL"
+    )
+)
+
+#wstępne przetwarzanie
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, content_transformer(tolower))
+stoplistFile <- paste(
+  inputDir, 
+  "\\",
+  "stopwords_pl.txt",
+  sep = ""
+)
+stoplist <- readLines(
+  stoplistFile,
+  encoding = "UTF-8"
+)
+corpus <- tm_map(corpus, removeWords, stoplist)
+corpus <- tm_map(corpus, stripWhitespace)
