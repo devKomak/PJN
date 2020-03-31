bd6dd9b691079d0096b7b6ce8734006629f797ac
devKomak <komcio96@gmail.com>
2020-03-31 13:14:00 +0200

    KM 2020-03-31

--- scripts/script_2.R
+++ scripts/script_2.R
@@ -84,19 +84,19 @@ tdmTfBoundsMatrix <- as.matrix(tdmTfBounds)
 tdmTfidfBoundsMatrix <- as.matrix(tdmTfidfBounds)
 
 #eksport macierzy do pliku .csv
-matrixFile <- paste(
-  outputDir, 
-  "\\",
-  "tdmTfidfBounds.csv",
-  sep = ""
-)
-write.table(tdmTfidfBoundsMatrix, file = matrixFile, sep = ";", dec = ",", col.names = NA)
+#matrixFile <- paste(
+#  outputDir, 
+#  "\\",
+#  "tdmTfidfBounds.csv",
+#  sep = ""
+#)
+#write.table(tdmTfidfBoundsMatrix, file = matrixFile, sep = ";", dec = ",", col.names = NA)
 
 #skalowanie wielowymiarowe
-d <- dist(tdmTfidfBoundsMatrix)
+d <- dist(dtmTfidfBoundsMAtrix)
 fit <- cmdscale(d,eig=TRUE, k=2)
 x <- fit$points[,1]
 y <- fit$points[,2]
-plot(x, y, xlab="Coortinate 1", ylab="Coordinate 2",
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2",
      main="Metric MDS", type="n")
-text(x, y, labels = row.names(tdmTfidfBoundsMatrix), cex=.7)
+text(x, y, labels = row.names(dtmTfidfBoundsMAtrix), cex=.7)
